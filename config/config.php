a175819e30904f2651d35e1997b8ac71e9a4ba3f
Yanachko Ivailo DevriX <107855626+yanachkov-devix@users.noreply.github.com>
2022-08-09 11:55:23 +0300

    add config file

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+define('SSL_ENABLED', true);
+define('BASE URL', '/test/');
+
+define('DB_NAME', 'jobs_v_2');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+
+define('IMAGE_PATH', 'img/company-logo/');
+define('DOC_UPLOAD_PATH', 'uploads/');
+
+
+
