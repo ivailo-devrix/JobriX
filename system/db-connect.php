a7f9a45601cecdf3b41a5f7f3222f65c913ed8bd
Yanachko Ivailo DevriX <107855626+yanachkov-devix@users.noreply.github.com>
2022-08-09 11:59:06 +0300

    Create db-connect.php

---
+++ system/db-connect.php
@@ -0,0 +1,13 @@
+<?php
+require_once ($_SERVER['DOCUMENT_ROOT'].'/config/config.php');
+
+
+$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}else{
+    echo 'connect!';
+}
