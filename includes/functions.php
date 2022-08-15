7f61b90a69d5068cff01fc7afdde8917d63fec3e
Yanachko Ivailo DevriX <107855626+yanachkov-devix@users.noreply.github.com>
2022-08-15 19:35:09 +0300

    validate-domain
    add a function that returns a valid domain

--- includes/functions.php
+++ includes/functions.php
@@ -12,3 +12,32 @@ function date_difference($date_1, $difference_format = '%a')
 
     return $interval->format($difference_format);
 }
+
+
+/**
+ * Extract and Check domain validation
+ * @param string $domain_or_url <p>
+ * accepts url address or  domain
+ * </p>
+ * @return string with the valid domain if domain is valid and boolean 'FALSE' if the domain is not valid.
+ */
+function extract_valid_domaine($domain_or_url)
+{
+    //extract domain from url
+    $domain_or_url = str_replace('http://', '', $domain_or_url);
+    $domain_or_url = str_replace('https://', '', $domain_or_url);
+    $domain_or_url_arr = explode('/', $domain_or_url);
+    $domain = $domain_or_url_arr[0];
+
+    // check pattern
+    $valid_pattern = (preg_match("/^(?!\-)(?:(?:[a-zA-Z\d][a-zA-Z\d\-]{0,61})?[a-zA-Z\d]\.){1,126}(?!\d+)[a-zA-Z\d]{1,63}$/", $domain));
+
+    // check DNS records
+    $valid_dns = dns_get_record($domain);
+
+    if ($valid_pattern && $valid_dns) {
+        return $domain;
+    } else {
+        return false;
+    }
+}
