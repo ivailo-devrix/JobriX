9584da4ddb3de6ff32c7d362228830dd7bbe80a2
Yanachko Ivailo DevriX <107855626+yanachkov-devix@users.noreply.github.com>
2022-08-19 16:55:37 +0300

    fix for problem: if there is no data in the database for a given value
    $job = array(
    	'title'               => '',
    	'company_name'        => '',
    	'company_description' => '',
    	'company_site'        => '',
    	'publication_date'    => '',
    	'jobs_location'       => '',
    	'salary'              => '',
    	'description'         => '',
    	'id_user'             => '',
    );
    
    foreach ( $row as $key => $value ) {
    	if ( ! empty( $value ) && array_key_exists( $key, $job ) ) {
    		$job[ $key ] = $value;
    	}
    }

--- job-view.php
+++ job-view.php
@@ -1,27 +1,49 @@
 <?php
-require_once(dirname(__FILE__) . '/includes/required-includes.php'); //Necessary file containing: The necessary custom functions, Config file constants, DB connection
-
-$jobs_id = $_GET['id'];
-$sql = "SELECT jobs.*, user.company_name, user.company_site FROM jobs JOIN user ON user.id_user = jobs.id_user WHERE id_jobs = '$jobs_id'";
-$result = db_sql_run($sql);
-
-if (mysqli_num_rows($result) < 1) {
-    header('Location: ' . BASE_URL);
-}
-$row = mysqli_fetch_assoc($result);
-
-
-//params for include template
-$meta_title = $row['title'] . " | Jobrix.tk";
-
-//header template include
-require_once(dirname(__FILE__) . '/includes/theme-compat/header.php');
+require_once( dirname( __FILE__ ) . '/includes/required-includes.php' ); //Necessary file containing: The necessary custom functions, Config file constants, DB connection
 
 //Checks if a number is passed as a parameter in the _get request and if the given ID exists in the database
 $jobs_id = "";
-if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
-    header('Location: ' . BASE_URL);
-} ?>
+if ( ! isset( $_GET['id'] ) || ! is_numeric( $_GET['id'] ) ) {
+	header( 'Location: ' . BASE_URL );
+}
+
+$jobs_id = mysqli_real_escape_string( open_db_conn(), $_GET['id'] );
+$sql     = "SELECT jobs.*, user.company_name, user.company_site FROM jobs JOIN user ON user.id_user = jobs.id_user WHERE id_jobs = '$jobs_id'";
+$result  = db_sql_run( $sql );
+
+//redirect to the home page if there is no job matching such id in the database
+if ( mysqli_num_rows( $result ) < 1 ) {
+	header( 'Location: ' . BASE_URL );
+}
+
+$row = mysqli_fetch_assoc( $result );
+
+
+$job = array(
+	'title'               => '',
+	'company_name'        => '',
+	'company_description' => '',
+	'company_site'        => '',
+	'publication_date'    => '',
+	'jobs_location'       => '',
+	'salary'              => '',
+	'description'         => '',
+	'id_user'             => '',
+);
+
+foreach ( $row as $key => $value ) {
+	if ( ! empty( $value ) && array_key_exists( $key, $job ) ) {
+		$job[ $key ] = $value;
+	}
+}
+
+
+//params for include template
+$meta_title = $job['title'] . " | Jobrix.tk";
+
+//header template include
+require_once( dirname( __FILE__ ) . '/includes/theme-compat/header.php' );
+?>
 <section class="section-fullwidth">
     <div class="row">
         <div class="job-single">
@@ -29,15 +51,22 @@ if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
                 <div class="job-card">
                     <div class="job-primary">
                         <header class="job-header">
-                            <h2 class="job-title"><?php echo $row['title'] ?></h2>
+                            <h2 class="job-title"><?php echo $job['title'] ?></h2>
                             <div class="job-meta">
-                                <a class="meta-company" href="#"><?php echo $row['company_name'] ?></a>
+								<?php
+								if ( ! empty( $job['company_site'] ) ) { ?>
+                                    <a class="meta-company"
+                                       href="<?php echo 'http://' . $job['company_site'] ?>"><?php echo $job['company_name'] ?></a>
+								<?php } else {
+									echo $job['company_name'];
+								}
+								?>
                                 <span class="meta-date">
                                         <?php
-                                        if (date_difference($row['publication_date']) < 31) {
-                                            echo "Posted " . date_difference($row['publication_date']) . " days ago";
+                                        if ( date_difference( $job['publication_date'] ) < 31 ) {
+	                                        echo "Posted " . date_difference( $job['publication_date'] ) . " days ago";
                                         } else {
-                                            echo "Posted over 1 month ago";
+	                                        echo "Posted over 1 month ago";
                                         }
                                         ?>
                                     </span>
@@ -45,10 +74,10 @@ if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
                             <div class="job-details">
                                     <span class="job-location">
                                         <?php
-                                        if (!empty($row['jobs_location'])) {
-                                            echo "Job location: " . $row['jobs_location'];
+                                        if ( ! empty( $job['jobs_location'] ) ) {
+	                                        echo "Job location: " . $job['jobs_location'];
                                         } else {
-                                            echo "No location specified";
+	                                        echo "No location specified";
                                         }
                                         ?>
                                     </span>
@@ -57,9 +86,9 @@ if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
                             </div>
                         </header>
                         <div class="job-body">
-                            <?php
-                            echo $row['description'];
-                            ?>
+							<?php
+							echo $job['description'];
+							?>
                         </div>
                     </div>
                 </div>
@@ -67,27 +96,27 @@ if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
             <aside class="job-secondary">
                 <div class="job-logo">
                     <div class="job-logo-box">
-                        <?php
-                        $img_file_logo = "./img/company/" . $row['id_user'] . ".jpg";
-                        if (file_exists($img_file_logo)) {
-                            echo '<img src="' . BASE_URL . "/img/company/" . $row['id_user'] . ".jpg" . '" alt="">';
-                        } else {
-                            echo '<img src="' . BASE_URL . "/img/company/" . "404.gif" . '" alt="">';
-                        }
-                        ?>
+						<?php
+						$img_file_logo = "./img/company/" . $job['id_user'] . ".jpg";
+						if ( file_exists( $img_file_logo ) ) {
+							echo '<img src="' . BASE_URL . "/img/company/" . $job['id_user'] . ".jpg" . '" alt="company logo">';
+						} else {
+							echo '<img src="' . BASE_URL . "/img/company/" . "404.gif" . '" alt="company logo">';
+						}
+						?>
                     </div>
                 </div>
                 <a href="#" class="button button-wide">Apply now</a>
-                <?php
-                if (!empty($row['company_site'])) { ?>
-                    <a href="<?php echo "http://" . $row['company_site']; ?>"
-                       target="_blank"><?php echo $row['company_site']; ?></a>
-                <?php }
-                ?>
+				<?php
+				if ( ! empty( $job['company_site'] ) ) { ?>
+                    <a href="<?php echo "http://" . $job['company_site']; ?>"
+                       target="_blank"><?php echo $job['company_site']; ?></a>
+				<?php }
+				?>
             </aside>
         </div>
     </div>
 </section>
 <?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/theme-compat/footer.php');
+require_once( $_SERVER['DOCUMENT_ROOT'] . '/includes/theme-compat/footer.php' );
 ?>
