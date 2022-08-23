4184126ed39c1afcf3e1e0aa9260e3c25eb5f71f
Yanachko Ivailo DevriX <107855626+yanachkov-devix@users.noreply.github.com>
2022-08-23 15:48:45 +0300

    FIX recommendation
    https://github.com/yanachkov-devix/JobriX/pull/11#discussion_r949353718

--- includes/functions.php
+++ includes/functions.php
@@ -1,15 +1,15 @@
 <?php
 
-date_default_timezone_set(DEF_TIME_ZONE);
+date_default_timezone_set( DEF_TIME_ZONE );
 
 
-function date_difference($date_1, $difference_format = '%a') {
-    $datetime1 = date_create($date_1);
-    $datetime2 = date_create();
+function date_difference( $date_1, $difference_format = '%a' ) {
+	$datetime1 = date_create( $date_1 );
+	$datetime2 = date_create();
 
-    $interval = date_diff($datetime2, $datetime1);
+	$interval = date_diff( $datetime2, $datetime1 );
 
-    return $interval->format($difference_format);
+	return $interval->format( $difference_format );
 }
 
 
@@ -20,24 +20,24 @@ function date_difference($date_1, $difference_format = '%a') {
  *
  * @return bool|string With the valid domain if domain is valid and boolean 'FALSE' if the domain is not valid.
  */
-function extract_valid_domaine($domain_or_url) {
-    //extract domain from url
-    $domain_or_url = trim($domain_or_url);
-    $domain_or_url = str_replace(array('http://', 'https://'),'', $domain_or_url);
-    $domain_or_url_arr = explode('/', $domain_or_url);
-    $domain = trim($domain_or_url_arr[0]);
+function extract_valid_domaine( $domain_or_url ) {
+	//extract domain from url
+	$domain_or_url     = trim( $domain_or_url );
+	$domain_or_url     = str_replace( array( 'http://', 'https://' ), '', $domain_or_url );
+	$domain_or_url_arr = explode( '/', $domain_or_url );
+	$domain            = trim( $domain_or_url_arr[0] );
 
-    // check pattern
-    $valid_pattern = (preg_match("/^(?!\-)(?:(?:[a-zA-Z\d][a-zA-Z\d\-]{0,61})?[a-zA-Z\d]\.){1,126}(?!\d+)[a-zA-Z\d]{1,63}$/", $domain));
+	// check pattern
+	$valid_pattern = ( preg_match( "/^(?!\-)(?:(?:[a-zA-Z\d][a-zA-Z\d\-]{0,61})?[a-zA-Z\d]\.){1,126}(?!\d+)[a-zA-Z\d]{1,63}$/", $domain ) );
 
-    // check DNS records
-    $valid_dns = dns_get_record($domain);
+	// check DNS records
+	$valid_dns = dns_get_record( $domain );
 
-    if ($valid_pattern && $valid_dns) {
-        return $domain;
-    } else {
-        return false;
-    }
+	if ( $valid_pattern && $valid_dns ) {
+		return $domain;
+	} else {
+		return false;
+	}
 }
 
 
@@ -48,16 +48,16 @@ function extract_valid_domaine($domain_or_url) {
  *
  * @return string With the valid Phone if Phone is valid and boolean 'FALSE' if the Phone is not valid.
  */
-function extract_valid_phone($phone) {
-    //remove chars - / ( ) and space
-    $phone = trim($phone);
-    $phone = str_replace(array(' ', '(', ')', '-', '/', '\\'), '', $phone);
+function extract_valid_phone( $phone ) {
+	//remove chars - / ( ) and space
+	$phone = trim( $phone );
+	$phone = str_replace( array( ' ', '(', ')', '-', '/', '\\' ), '', $phone );
 
-    if (preg_match('/^(\+359|0)\s?8(\d{2}\s\d{3}\d{3}|[789]\d{7})$/', $phone)) {
-        return str_replace('+359', 0, $phone);
-    } else {
-        return false;
-    }
+	if ( preg_match( '/^(\+359|0)\s?8(\d{2}\s\d{3}\d{3}|[789]\d{7})$/', $phone ) ) {
+		return str_replace( '+359', 0, $phone );
+	} else {
+		return false;
+	}
 }
 
 
@@ -80,13 +80,12 @@ EOD;
 		} elseif ( 1 == $i ) {
 			$active = ' current';
 		}
-		if (!empty($_GET) && empty($_GET['page'])){
+		if ( ! empty( $_GET ) && empty( $_GET['page'] ) ) {
 			$concatenate = '&';
-		}
-		else{
+		} else {
 			$concatenate = '?';
 		}
-		echo '<a class="page-numbers' . $active . '" href="' . $page_url . $concatenate.'page=' . $i . '">' . $i . '</a>';
+		echo '<a class="page-numbers' . $active . '" href="' . $page_url . $concatenate . 'page=' . $i . '">' . $i . '</a>';
 	}
 
 	$var = <<<EOD
@@ -96,4 +95,38 @@ EOD;
 EOD;
 	echo $var;
 
+}
+
+
+function active_jobs() {
+	$sql = "SELECT COUNT(status)
+			FROM jobs
+			WHERE status = 'active'; ";
+
+	$result = db_sql_run( $sql );
+	$result = mysqli_fetch_assoc( $result );
+
+	return $result['COUNT(status)'];
+}
+
+
+function get_jobs( $start_index, $search ) {
+	if ( ! empty( $search ) ) {
+		$sql = "SELECT jobs.*, user.company_name, user.company_site 
+				FROM `jobs` JOIN user ON user.id_user = jobs.id_user 
+				WHERE ( CONVERT(`title` USING utf8) LIKE '%" . $search . "%' AND `status` = 'active') 
+				LIMIT 0,20";
+	} else {
+		$sql = "SELECT jobs.*, user.company_name, user.company_site 
+				FROM jobs JOIN user ON user.id_user = jobs.id_user 
+				WHERE status = 'active' 
+				LIMIT " . $start_index . "," . JOBS_PER_PAGE;
+	}
+
+	return db_sql_run( $sql );
+}
+
+
+function jobs_start_index_for_page( $page ) {
+	return ( $page * JOBS_PER_PAGE ) - JOBS_PER_PAGE;
 }
--- index.php
+++ index.php
@@ -1,27 +1,11 @@
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/theme-compat/header.php');
+<?php require_once( $_SERVER['DOCUMENT_ROOT'] . '/includes/theme-compat/header.php' );
 
 
-$sql = "SELECT COUNT(status)
-FROM jobs
-WHERE status = 'active'; ";
-
-$result = db_sql_run($sql);
-$result = mysqli_fetch_assoc($result);
-$active_jobs = $result['COUNT(status)'];
 
 $start_index = 0;
-
-
-function jobs_start_index_for_page($page) {
-    return ($page * JOBS_PER_PAGE) - JOBS_PER_PAGE;
-}
-
-if (!empty($_GET['page'])) {
-    $start_index = jobs_start_index_for_page($_GET['page']);
-}
-
-?>
+if ( ! empty( $_GET['page'] ) ) {
+	$start_index = jobs_start_index_for_page( $_GET['page'] );
+} ?>
     <section class="section-fullwidth section-jobs-preview">
         <div class="row">
             <ul class="tags-list">
@@ -42,14 +26,14 @@ if (!empty($_GET['page'])) {
                 </li>
             </ul>
 
-
             <div class="flex-container centered-vertically">
                 <div class="search-form-wrapper">
                     <form class="example" method="GET">
                         <div class="search-form-field">
                             <input class="search-form-input" type="text"
-                                   value="<?php if (isset($_GET['search'])) echo $_GET['search']; ?>"
-                                   placeholder="Search…" name="search">
+                                   value="<?php if ( isset( $_GET['search'] ) ) {
+								       echo $_GET['search'];
+							       } ?>" placeholder="Search…" name="search">
                         </div>
                 </div>
                 <div class="filter-wrapper">
@@ -64,80 +48,66 @@ if (!empty($_GET['page'])) {
                     </form>
                 </div>
             </div>
+			<?php $search = '';
+			if ( ! empty( $_GET['search'] ) ) {
+				$search = $_GET['search'];
+			}
 
-            <?php
+			$result = get_jobs( $start_index, $search );
 
-            // $sql = "SELECT * FROM `jobs` WHERE ( CONVERT(`title` USING utf8) LIKE '%продавач%' AND `status` = 'active') LIMIT 0,2";
-            if (!empty($_GET['search'])) {
-                $search = $_GET['search'];
-                $sql = "SELECT jobs.*, user.company_name, user.company_site FROM `jobs` JOIN user ON user.id_user = jobs.id_user WHERE ( CONVERT(`title` USING utf8) LIKE '%" . $search . "%' AND `status` = 'active') LIMIT 0,20";
-            } else {
-                $sql = "SELECT jobs.*, user.company_name, user.company_site FROM jobs JOIN user ON user.id_user = jobs.id_user where status = 'active' LIMIT " . $start_index . "," . JOBS_PER_PAGE;
-            }
+			if ( mysqli_num_rows( $result ) > 0 ) {
+				// output data of each row
+				while ( $row = mysqli_fetch_assoc( $result ) ) { ?>
+                    <ul class="jobs-listing">
+                        <li class="job-card">
+                            <div class="job-primary">
+                                <h2 class="job-title"><a
+                                            href="<?php echo BASE_URL . "/job-view.php?id=" . $row['id_jobs'] ?>"><?php echo $row['title']; ?></a>
+                                </h2>
+                                <div class="job-meta">
+                                    <a class="meta-company"
+                                       href="<?php echo BASE_URL . "/job-view.php?id=" . $row['id_jobs'] ?>"><?php echo $row['company_name']; ?></a>
+                                    <span class="meta-date">
+                                        <?php if ( date_difference( $row['publication_date'] ) < 31 ) {
+											echo "Posted " . date_difference( $row['publication_date'] ) . " days ago";
+										} else {
+											echo "Posted over 1 month ago";
+										} ?>
+                                    </span>
+                                </div>
+                                <div class="job-details">
+                                <span class="job-location">
+                                    <?php if ( ! empty( $row['jobs_location'] ) ) {
+	                                    echo "Job location: " . $row['jobs_location'];
+                                    } else {
+	                                    echo "No location specified";
+                                    } ?>
+                                </span>
+                                    <span class="job-type">Job Type: Contract staff</span>
+                                </div>
+                            </div>
+                            <div class="job-logo">
+                                <div class="job-logo-box">
+									<?php $img_file_logo = "." . IMAGE_PATH . $row['id_user'] . ".jpg";
+									if ( file_exists( $img_file_logo ) ) {
+										echo '<img src="' . BASE_URL . IMAGE_PATH . $row['id_user'] . ".jpg" . '" alt="">';
+									} ?>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+				<?php }
+			} else {
+				echo "0 results";
+			}
 
-
-            $result = db_sql_run($sql);
-
-            if (mysqli_num_rows($result) > 0) {
-            // output data of each row
-            while ($row = mysqli_fetch_assoc($result)) {
-            ?>
-            <ul class="jobs-listing">
-                <li class="job-card">
-                    <div class="job-primary">
-                        <h2 class="job-title"><a
-                                    href="<?php echo BASE_URL . "/job-view.php?id=" . $row['id_jobs'] ?>"><?php echo $row['title']; ?></a>
-                        </h2>
-                        <div class="job-meta">
-                            <a class="meta-company"
-                               href="<?php echo BASE_URL . "/job-view.php?id=" . $row['id_jobs'] ?>"><?php echo $row['company_name']; ?></a>
-                            <span class="meta-date"><?php if (date_difference($row['publication_date']) < 31) {
-                                    echo "Posted " . date_difference($row['publication_date']) . " days ago";
-                                } else {
-                                    echo "Posted over 1 month ago";
-                                } ?></span>
-                        </div>
-                        <div class="job-details">
-                        <span class="job-location"><?php
-                            if (!empty($row['jobs_location'])) {
-                                echo "Job location: " . $row['jobs_location'];
-                            } else {
-                                echo "No location specified";
-                            }
-                            ?></span>
-                            <span class="job-type">Job Type: Contract staff</span>
-                        </div>
-                    </div>
-                    <div class="job-logo">
-                        <div class="job-logo-box">
-                            <?php
-                            $img_file_logo = "." . IMAGE_PATH . $row['id_user'] . ".jpg";
-                            if (file_exists($img_file_logo)) {
-                                echo '<img src="' . BASE_URL . IMAGE_PATH . $row['id_user'] . ".jpg" . '" alt="">';
-                            }
-                            ?>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-                <?php
-                }
-                } else {
-                    echo "0 results";
-                }
-                ?>
-
-
-
-            <?php
-            $s_parameter = '';
-            if (!empty($_GET['search'])){
-	            $s_parameter = '?search=' . $_GET['search'];
-            }
-            $page_url = 'index.php' . $s_parameter;
-            pagination($active_jobs,$page_url); ?>
+			$s_parameter = '';
+			if ( ! empty( $_GET['search'] ) ) {
+				$s_parameter = '?search=' . $_GET['search'];
+			}
+			$page_url = 'index.php' . $s_parameter;
+			$active_jobs = active_jobs();
+			pagination( $active_jobs, $page_url ); ?>
         </div>
     </section>
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/includes/theme-compat/footer.php');
-?>
+<?php require_once( $_SERVER['DOCUMENT_ROOT'] . '/includes/theme-compat/footer.php' ); ?>
