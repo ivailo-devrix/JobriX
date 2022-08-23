4d2644eb804a8be99c9d8b98ff24c2176b5da031
Yanachko Ivailo DevriX <107855626+yanachkov-devix@users.noreply.github.com>
2022-08-23 16:03:57 +0300

    add mysqli_real_escape_string for _get parameters

--- index.php
+++ index.php
@@ -1,81 +1,82 @@
 <?php require_once( $_SERVER['DOCUMENT_ROOT'] . '/includes/theme-compat/header.php' );
 
-
+$search = '';
+if ( isset( $_GET['search'] ) ) {
+	$search = mysqli_real_escape_string( open_db_conn(), $_GET['search'] );
+	$search = htmlspecialchars( $search );
+}
 
 $start_index = 0;
 if ( ! empty( $_GET['page'] ) ) {
-	$start_index = jobs_start_index_for_page( $_GET['page'] );
-} ?>
-    <section class="section-fullwidth section-jobs-preview">
-        <div class="row">
-            <ul class="tags-list">
-                <li class="list-item">
-                    <a href="#" class="list-item-link">IT</a>
-                </li>
-                <li class="list-item">
-                    <a href="#" class="list-item-link">Manufactoring</a>
-                </li>
-                <li class="list-item">
-                    <a href="#" class="list-item-link">Commerce</a>
-                </li>
-                <li class="list-item">
-                    <a href="#" class="list-item-link">Architecture</a>
-                </li>
-                <li class="list-item">
-                    <a href="#" class="list-item-link">Marketing</a>
-                </li>
-            </ul>
+	$page_num    = mysqli_real_escape_string( open_db_conn(), $_GET['page'] );
+	$start_index = jobs_start_index_for_page( $page_num );
+}
 
-            <div class="flex-container centered-vertically">
-                <div class="search-form-wrapper">
-                    <form class="example" method="GET">
-                        <div class="search-form-field">
-                            <input class="search-form-input" type="text"
-                                   value="<?php if ( isset( $_GET['search'] ) ) {
-								       echo $_GET['search'];
-							       } ?>" placeholder="Search…" name="search">
-                        </div>
-                </div>
-                <div class="filter-wrapper">
-                    <div class="filter-field-wrapper">
-                        <select>
-                            <option value="1">Date</option>
-                            <option value="2">Date</option>
-                            <option value="3">Date</option>
-                            <option value="4">Type</option>
-                        </select>
+$jobs_list = get_jobs( $start_index, $search );
+
+?>
+<section class="section-fullwidth section-jobs-preview">
+    <div class="row">
+        <ul class="tags-list">
+            <li class="list-item">
+                <a href="#" class="list-item-link">IT</a>
+            </li>
+            <li class="list-item">
+                <a href="#" class="list-item-link">Manufactoring</a>
+            </li>
+            <li class="list-item">
+                <a href="#" class="list-item-link">Commerce</a>
+            </li>
+            <li class="list-item">
+                <a href="#" class="list-item-link">Architecture</a>
+            </li>
+            <li class="list-item">
+                <a href="#" class="list-item-link">Marketing</a>
+            </li>
+        </ul>
+        <div class="flex-container centered-vertically">
+            <div class="search-form-wrapper">
+                <form class="example" method="GET">
+                    <div class="search-form-field">
+                        <input class="search-form-input" type="text"
+                               value="<?php if ( ! empty( $search ) ) {
+							       echo $search;
+						       } ?>" placeholder="Search…" name="search">
                     </div>
-                    </form>
-                </div>
             </div>
-			<?php $search = '';
-			if ( ! empty( $_GET['search'] ) ) {
-				$search = $_GET['search'];
-			}
-
-			$result = get_jobs( $start_index, $search );
-
-			if ( mysqli_num_rows( $result ) > 0 ) {
-				// output data of each row
-				while ( $row = mysqli_fetch_assoc( $result ) ) { ?>
-                    <ul class="jobs-listing">
-                        <li class="job-card">
-                            <div class="job-primary">
-                                <h2 class="job-title"><a
-                                            href="<?php echo BASE_URL . "/job-view.php?id=" . $row['id_jobs'] ?>"><?php echo $row['title']; ?></a>
-                                </h2>
-                                <div class="job-meta">
-                                    <a class="meta-company"
-                                       href="<?php echo BASE_URL . "/job-view.php?id=" . $row['id_jobs'] ?>"><?php echo $row['company_name']; ?></a>
-                                    <span class="meta-date">
+            <div class="filter-wrapper">
+                <div class="filter-field-wrapper">
+                    <select>
+                        <option value="1">Date</option>
+                        <option value="2">Date</option>
+                        <option value="3">Date</option>
+                        <option value="4">Type</option>
+                    </select>
+                </div>
+                </form>
+            </div>
+        </div>
+		<?php if ( mysqli_num_rows( $jobs_list ) > 0 ) {
+			// output data of each row
+			while ( $row = mysqli_fetch_assoc( $jobs_list ) ) { ?>
+                <ul class="jobs-listing">
+                    <li class="job-card">
+                        <div class="job-primary">
+                            <h2 class="job-title"><a
+                                        href="<?php echo BASE_URL . "/job-view.php?id=" . $row['id_jobs'] ?>"><?php echo $row['title']; ?></a>
+                            </h2>
+                            <div class="job-meta">
+                                <a class="meta-company"
+                                   href="<?php echo BASE_URL . "/job-view.php?id=" . $row['id_jobs'] ?>"><?php echo $row['company_name']; ?></a>
+                                <span class="meta-date">
                                         <?php if ( date_difference( $row['publication_date'] ) < 31 ) {
-											echo "Posted " . date_difference( $row['publication_date'] ) . " days ago";
-										} else {
-											echo "Posted over 1 month ago";
-										} ?>
+	                                        echo "Posted " . date_difference( $row['publication_date'] ) . " days ago";
+                                        } else {
+	                                        echo "Posted over 1 month ago";
+                                        } ?>
                                     </span>
-                                </div>
-                                <div class="job-details">
+                            </div>
+                            <div class="job-details">
                                 <span class="job-location">
                                     <?php if ( ! empty( $row['jobs_location'] ) ) {
 	                                    echo "Job location: " . $row['jobs_location'];
@@ -83,31 +84,31 @@ if ( ! empty( $_GET['page'] ) ) {
 	                                    echo "No location specified";
                                     } ?>
                                 </span>
-                                    <span class="job-type">Job Type: Contract staff</span>
-                                </div>
+                                <span class="job-type">Job Type: Contract staff</span>
                             </div>
-                            <div class="job-logo">
-                                <div class="job-logo-box">
-									<?php $img_file_logo = "." . IMAGE_PATH . $row['id_user'] . ".jpg";
-									if ( file_exists( $img_file_logo ) ) {
-										echo '<img src="' . BASE_URL . IMAGE_PATH . $row['id_user'] . ".jpg" . '" alt="">';
-									} ?>
-                                </div>
+                        </div>
+                        <div class="job-logo">
+                            <div class="job-logo-box">
+								<?php $img_file_logo = "." . IMAGE_PATH . $row['id_user'] . ".jpg";
+								if ( file_exists( $img_file_logo ) ) {
+									echo '<img src="' . BASE_URL . IMAGE_PATH . $row['id_user'] . ".jpg" . '" alt="">';
+								} ?>
                             </div>
-                        </li>
-                    </ul>
-				<?php }
-			} else {
-				echo "0 results";
-			}
+                        </div>
+                    </li>
+                </ul>
+			<?php }
+		} else {
+			echo "0 results";
+		}
 
-			$s_parameter = '';
-			if ( ! empty( $_GET['search'] ) ) {
-				$s_parameter = '?search=' . $_GET['search'];
-			}
-			$page_url = 'index.php' . $s_parameter;
-			$active_jobs = active_jobs();
-			pagination( $active_jobs, $page_url ); ?>
-        </div>
-    </section>
+		$s_parameter = '';
+		if ( ! empty( $search ) ) {
+			$s_parameter = '?search=' . $search;
+		}
+		$page_url    = 'index.php' . $s_parameter;
+		$active_jobs = active_jobs();
+		pagination( $active_jobs, $page_url ); ?>
+    </div>
+</section>
 <?php require_once( $_SERVER['DOCUMENT_ROOT'] . '/includes/theme-compat/footer.php' ); ?>
