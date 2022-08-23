5a6da7671859464d875eaf97963c7e59c3ce0917
Yanachko Ivailo DevriX <107855626+yanachkov-devix@users.noreply.github.com>
2022-08-23 17:01:10 +0300

    move inline style to css / reformatting code

--- includes/theme-compat/register-profile.php
+++ includes/theme-compat/register-profile.php
@@ -16,84 +16,80 @@
                             <div class="form-field-wrapper">
                                 <input type="text" name="first_name" placeholder="First Name*"
                                        <?php if (!empty($_SESSION['first_name'])) {
-                                           echo 'value="' . $_SESSION['first_name'] . '" style="color: #3c71fe;" ';
-                                       } ?>required>
+                                           echo 'value="' . $_SESSION['first_name'] . '" class="correct" ';
+                                       } ?> required>
                             </div>
                             <div class="form-field-wrapper">
                                 <input type="text" name="last_name"
                                        placeholder="Last Name*" <?php if (!empty($_SESSION['last_name'])) {
-                                    echo 'value="' . $_SESSION['last_name'] . '" style="color: #3c71fe;" ';
+                                    echo 'value="' . $_SESSION['last_name'] . '" class="correct" ';
                                 } ?> required>
                             </div>
                             <div class="form-field-wrapper">
                                 <input type="email" name="email"
                                        placeholder="Email*" <?php if (!empty($_SESSION['email'])) {
-                                    echo 'value="' . $_SESSION['email'] . '" style="color: #3c71fe;" ';
+                                    echo 'value="' . $_SESSION['email'] . '" class="correct" ';
                                 } ?> required>
                             </div>
                             <?php if (!empty($page_name) && $page_name == 'register') { ?>
                                 <div class="form-field-wrapper">
                                     <input type="password" name="password" placeholder="Password*" required>
-                                </div><?php } ?>
-                            <?php if (!empty($page_name) && $page_name == 'register') { ?>
+                                </div><?php }
+                            if (!empty($page_name) && $page_name == 'register') { ?>
                                 <div class="form-field-wrapper">
                                     <input type="password" name="r_password" placeholder="Repeat Password*" required>
-                                </div><?php } ?>
-                            <?php if (!empty($page_name) && $page_name == 'profile') { ?>
+                                </div><?php }
+                            if (!empty($page_name) && $page_name == 'profile') { ?>
                                 <div class="form-field-wrapper">
-                                    <input type="password"
-                                           name="password"
-                                           placeholder="Password">
-                                </div><?php } ?>
-                            <?php if (!empty($page_name) && $page_name == 'profile') { ?>
+                                    <input type="password" name="password" placeholder="Password">
+                                </div><?php }
+                            if (!empty($page_name) && $page_name == 'profile') { ?>
                                 <div class="form-field-wrapper">
                                     <input type="password" name="r_password" placeholder="Repeat Password">
                                 </div><?php } ?>
                             <div class="form-field-wrapper">
-                                <input type="text" name="phone"
-                                       placeholder="Phone Number" <?php if (!empty($_SESSION['input_phone'])) {
-                                    echo 'value="' . $_SESSION['input_phone'] . '" style="color: #3c71fe;" ';
-                                } ?>
-                                    <?php if (!empty($_SESSION['phone'])) {
-                                        echo 'value="' . $_SESSION['phone'] . '" style="color: #3c71fe;" ';
-                                    } ?> >
+                                <input type="text" name="phone" placeholder="Phone Number" <?php if (!empty($_SESSION['input_phone'])) {
+                                    echo 'value="' . $_SESSION['input_phone'] . '" class="correct" ';
+                                }
+                                if (!empty($_SESSION['phone'])) {
+                                    echo 'value="' . $_SESSION['phone'] . '" class="correct" ';
+                                    } ?>
+                                >
                             </div>
                         </div>
                         <div class="secondary-container">
                             <h4 class="form-title">My Company</h4>
                             <div class="form-field-wrapper">
-                                <input type="text" name="company_name" placeholder="Company Name" <input
-                                        type="text"
-                                        name="phone"
-                                        placeholder="Phone Number" <?php if (!empty($_SESSION['company_name'])) {
-                                    echo 'value="' . $_SESSION['company_name'] . '" style="color: #3c71fe;" ';
+                                <input type="text" name="company_name" placeholder="Company Name" <input type="text" name="phone" placeholder="Phone Number"
+                                    <?php if (!empty($_SESSION['company_name'])) {
+                                    echo 'value="' . $_SESSION['company_name'] . '" class="correct" ';
                                 } ?>
                                 >
                             </div>
                             <div class="form-field-wrapper">
-                                <input type="text" name="company_site"
-                                       placeholder="Company Site" <?php if (!empty($_SESSION['company_site'])) {
-                                    echo 'value="' . $_SESSION['company_site'] . '" style="color: #3c71fe;" ';
+                                <input type="text" name="company_site" placeholder="Company Site" <?php if (!empty($_SESSION['company_site'])) {
+                                    echo 'value="' . $_SESSION['company_site'] . '" class="correct" ';
                                 } ?>
                                 >
                             </div>
                             <div class="form-field-wrapper">
-                                    <textarea name="company_description"
-                                              placeholder="Description" <?php if (!empty($_SESSION['company_description'])) {
-                                        echo '" style="color: #3c71fe;" ';
-                                    } ?>><?php if (!empty($_SESSION['company_description'])) {
-                                            echo $_SESSION['company_description'];
-                                        }
-                                        ?></textarea>
+                                    <textarea name="company_description" placeholder="Description"
+                                        <?php if (!empty($_SESSION['company_description'])) {
+                                        echo '" class="correct" ';
+                                    } ?>
+                                    >
+                                    <?php if (!empty($_SESSION['company_description'])) {
+                                        echo $_SESSION['company_description'];
+                                    } ?>
+                                    </textarea>
                             </div>
                         </div>
                     </div>
                     <?php if (!empty($_SESSION['errors'])) {
                         foreach ($_SESSION['errors'] as $error) {
-                            echo "<p style=\"color: red;\"> $error </p>";
+                            echo '<p class="error">'. $error . '</p>';
                         }
-                    }
-                    ?>
+                    } ?>
                     <button class="button">
                         Register
                     </button>
